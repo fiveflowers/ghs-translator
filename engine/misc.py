ccfa3f17955519e1924feb126bb9a6afd6000978
Yifer Huang <yifer.me@foxmail.com>
2020-10-22 19:20:14 +0800

    增加"格式化字符串"功能

--- engine/misc.py
+++ engine/misc.py
@@ -3,9 +3,14 @@
 # @Time    : 2020/10/13
 # @Author  : Yifer Huang
 # @File    : misc.py
-# @Desc    : 一些杂项函数
+# @Desc    : 杂项函数
 
 
 def format_text(text):
+    """
+    格式化字符串, 主要用于处理 PDF 复制的换行
+    :param text: 输入的原始文本
+    :return: 格式化后的文本
+    """
     text = text.replace('\n', " ")
     return text
