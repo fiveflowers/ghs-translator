150c60b35823547360750eacb7f570b554354ffa
Yifer Huang <yifer.me@foxmail.com>
2020-10-22 21:01:28 +0800

    删除"有道翻译",太拉跨了

--- engine/translator.py
+++ engine/translator.py
@@ -27,25 +27,6 @@ class GoogleTrans(Translator):
         return super(GoogleTrans, self).translate(text=text, dest=dest_lang, src=src_lang).text
 
 
-class YouDaoTrans:
-    def __init__(self):
-        pass
-
-    def trans(self, text, src_lang='auto', dest_lang='zh-cn'):
-        """
-        标准化翻译接口
-
-        Args:
-            text: 待翻译的文本
-            src_lang: 源语言
-            dest_lang: 目的语言
-
-        Returns: 翻译好的文本
-
-        """
-        return "有道翻译服务即将上线..."
-
-
 class BaiDuTrans:
     def __init__(self):
         pass
