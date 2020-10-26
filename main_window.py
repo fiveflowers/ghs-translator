3c7c4c740e102b7f8b3289d42b9d4c133d9463c8
Yifer Huang <yifer.me@foxmail.com>
2020-10-26 13:24:10 +0800

    add: format button

--- main_window.py
+++ main_window.py
@@ -14,7 +14,7 @@ from PyQt5.QtGui import QKeySequence, QIcon
 from PyQt5.QtWidgets import QMainWindow, QApplication, QShortcut, QSystemTrayIcon
 
 from engine import misc
-from engine.cross_platform import HOT_KEY_MINIMIZED, HOT_KEY_CLOSE
+from engine.cross_platform import HOT_KEY_MINIMIZED, HOT_KEY_CLOSE, TEXT_SRC, TEXT_DEST
 from engine.translator import GoogleTrans, BaiDuTrans
 from main_window_layout import Ui_MainWindow
 
@@ -28,13 +28,22 @@ class MainWindow(QMainWindow):
         self.platform = platform.system()  # 获取当前系统
         self.ui = Ui_MainWindow()  # 实例化UI
         self.ui.setupUi(self)  # 给主窗口加载UI
+
+        # 界面的一些初始化操作
         self.setWindowFlags(QtCore.Qt.WindowStaysOnTopHint)  # 窗口置顶
         self.connect_signal_and_slot()  # 绑定信号和槽
+        self.init_customized_ui()
+
+        # 一些全局变量
         self.FLAG = False  # 翻译服务 全局 Flag
         self.last_trans_time = time.time()  # 存储上次翻译的时间戳
-        self.trans_thread = threading.Thread(target=self.action)  # 新建子线程,用于计时
+
+        # 子线程
+        self.trans_thread = threading.Thread(target=self.action)  # 新建子线程
         self.trans_thread.setDaemon(True)  # 守护进程
         self.trans_thread.start()  # 启动线程
+
+        # 翻译器API
         self.mytranslator = {
             '谷歌': GoogleTrans(),
             '百度': BaiDuTrans(),
@@ -58,6 +67,13 @@ class MainWindow(QMainWindow):
         self.ui.pushButton_copy.clicked.connect(self.slot_copy_text)
         self.signal_thread.connect(self.slot_translate)
 
+    def init_customized_ui(self):
+        """
+        初始化一些自定义的UI
+        """
+        self.ui.plainTextEdit_src.setPlainText(TEXT_SRC[self.platform])
+        self.ui.plainTextEdit_dest.setPlainText(TEXT_DEST[self.platform])
+
     def slot_translate(self):
         """
         槽： 翻译服务
@@ -70,15 +86,18 @@ class MainWindow(QMainWindow):
         }
         max_len = 2000  # 单次翻译字符上限
         src_text = self.ui.plainTextEdit_src.toPlainText()  # 获取src框内文本
+        src_lang = self.ui.comboBox_src_lang.currentText()[-2:]  # 获取src语言
 
-        if src_text == '' or len(src_text) > max_len:  # 空字符或者超过字数限制调用翻译
+        # 空字符或者超过字数限制调用翻译
+        if src_text == '' or len(src_text) > max_len or src_text == TEXT_SRC[self.platform]:
             return
 
-        src_text_formatted = misc.format_text(src_text)  # 格式化文本
-        src_lang = self.ui.comboBox_src_lang.currentText()[-2:]  # 获取src语言
+        # 格式化文本
+        src_text_formatted = misc.format_text(src_text) if self.ui.pushButton_format.isChecked() else src_text
         if src_text != src_text_formatted:
             self.ui.plainTextEdit_src.setPlainText(src_text_formatted)  # 显示格式化后的文本（去换行）
 
+        # 翻译并显示
         api_type = self.ui.comboBox_trans_api.currentText()  # 获取选用的翻译API
         dest_lang = self.ui.comboBox_dest_lang.currentText()[-2:]  # 获取dest语言
         dest_text = self.mytranslator[api_type].trans(src_text_formatted, lang_dict[src_lang],
